943bd31983b7b7e7ab664fc061032955492258ce
onthered <jbuck594@gmail.com>
2015-03-15 01:02:23 -0400

    Setup Basics for Classes

--- MMU.java
+++ MMU.java
@@ -2,5 +2,38 @@ package binarybuddysystem;
 
 public class MMU
 {
+	Process[] memory; 	//Might make wrapper class (Chunk)
+						//Should it be a LinkedList instead?
 	
+	public MMU()
+	{
+		
+	}
+	
+	public boolean allocate(Process p)
+	{
+		return false;
+	}
+	
+	public boolean deallocate(String name)
+	{
+		return false;
+	}
+	
+	private boolean partition(int index)	//index or hole
+	{
+		return false;
+	}
+	
+	private boolean merge(int index)		//index or hole
+	{
+		return false;
+	}
+	
+	public String toString()
+	{
+		String str = "";
+		
+		return str;
+	}
 }
--- Process.java
+++ Process.java
@@ -2,5 +2,29 @@ package binarybuddysystem;
 
 public class Process
 {
+	private String name;
+	private int size;
+	/*
+	 * Should we have a buddy reference in Process
+	 * or make a wrapper class (Chunk) that would manage that?
+	 */
 	
+	
+	public Process()
+	{
+		name = "";
+		size = 0;
+	}
+	
+	public Process(String name, int size)
+	{
+		this.name = name;
+		this.size = size;
+	}
+	
+	public String toString()
+	{
+		String str = "";
+		return str;
+	}
 }
