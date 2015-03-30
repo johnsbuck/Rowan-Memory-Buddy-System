e03f8d1dc982182ed6f1197b7f0bc34ad53921a8
Aaron N. Rudolph <aanrudolph2@gmail.com>
2015-03-29 21:48:19 -0400

    Added JUnit Tests Fixed error with return value from allocate()

--- src/binarybuddysystem/MMU.java
+++ src/binarybuddysystem/MMU.java
@@ -80,7 +80,7 @@ public class MMU
 		}
 		
 		
-		return false;
+		return allocated;
 	}
 	
 	/**
--- src/binarybuddysystem/test/MMUTest.java
+++ src/binarybuddysystem/test/MMUTest.java
@@ -19,15 +19,49 @@ public class MMUTest
 	public void setUp()
 	{
 		testMMU = new MMU(1024, 64);
-		p1 = new Process("Process 1", 2);
-		p2 = new Process("Process 2", 63);
-		p3 = new Process("Process 3", 129);
+		p1 = new Process("Process 1", 2);	//  2B  (Fills 64)
+		p2 = new Process("Process 2", 63);	// 63B  (Fills 64)
+		p3 = new Process("Process 3", 129);	// 129B (Fills 256)
+		p4 = new Process("Process 4", 768);	// 768B	(Fills 1024)
 	}
 	
 	@Test
-	public void test()
+	public void AllocTest1()
 	{
-		fail("Not yet implemented");
+		assertTrue(testMMU.allocate(p1));
+		assertTrue(testMMU.allocate(p2));
+		assertTrue(testMMU.allocate(p3));
+		assertFalse(testMMU.allocate(p4));
+	}
+	
+	@Test
+	public void AllocTest2()
+	{
+		assertTrue(testMMU.allocate(p4));
+		assertFalse(testMMU.allocate(p1));
+	}
+	
+	@Test
+	public void AllocDeallocTest1()
+	{
+		assertTrue(testMMU.allocate(p4));
+		assertFalse(testMMU.allocate(p1));
+		
+		assertTrue(testMMU.deallocate("Process 4"));
+		
+		assertTrue(testMMU.allocate(p1));
+	}
+	
+	@Test
+	public void AllocDeallocTest2()
+	{
+		assertTrue(testMMU.allocate(p3));
+		
+		assertTrue(testMMU.deallocate("Process 3"));
+		
+		assertTrue(testMMU.allocate(p1));
+		
+		assertTrue(testMMU.deallocate("Process 1"));
 	}
 
 }
