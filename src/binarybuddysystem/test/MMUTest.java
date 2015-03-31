7442de26967a7be84e4d961b98abfdea4b5eb557
Aaron N. Rudolph <aanrudolph2@gmail.com>
2015-03-30 21:18:35 -0400

    Completed tests

--- src/binarybuddysystem/test/MMUTest.java
+++ src/binarybuddysystem/test/MMUTest.java
@@ -10,19 +10,18 @@ import org.junit.Test;
 public class MMUTest
 {
 	MMU testMMU;
-	Process p1;
-	Process p2;
-	Process p3;
-	Process p4;
+	Process p0, p1, p2, p3, p4, p5;
 	
 	@Before
 	public void setUp()
 	{
 		testMMU = new MMU(1024, 64);
+		p0 = new Process("Process 0", 32);	// 32B  (Fills 64)
 		p1 = new Process("Process 1", 2);	//  2B  (Fills 64)
-		p2 = new Process("Process 2", 63);	// 63B  (Fills 64)
+		p2 = new Process("Process 2", 63);	// 65B  (Fills 128)
 		p3 = new Process("Process 3", 129);	// 129B (Fills 256)
 		p4 = new Process("Process 4", 768);	// 768B	(Fills 1024)
+		p5 = new Process("Process 5", 257); // 257B (Fills 512)
 	}
 	
 	@Test
@@ -32,6 +31,7 @@ public class MMUTest
 		assertTrue(testMMU.allocate(p2));
 		assertTrue(testMMU.allocate(p3));
 		assertFalse(testMMU.allocate(p4));
+		assertTrue(testMMU.allocate(p5));
 	}
 	
 	@Test
@@ -50,6 +50,14 @@ public class MMUTest
 		assertTrue(testMMU.deallocate("Process 4"));
 		
 		assertTrue(testMMU.allocate(p1));
+		assertTrue(testMMU.allocate(p3));
+		assertTrue(testMMU.allocate(p5));
+		
+		assertTrue(testMMU.deallocate("Process 1"));
+		assertTrue(testMMU.deallocate("Process 3"));
+		assertTrue(testMMU.deallocate("Process 5"));
+		
+		assertTrue(testMMU.allocate(p4));
 	}
 	
 	@Test
@@ -57,11 +65,13 @@ public class MMUTest
 	{
 		assertTrue(testMMU.allocate(p3));
 		
-		assertTrue(testMMU.deallocate("Process 3"));
+		assertTrue(testMMU.allocate(p5));
 		
 		assertTrue(testMMU.allocate(p1));
 		
-		assertTrue(testMMU.deallocate("Process 1"));
+		assertTrue(testMMU.deallocate("Process 3"));
+		
+		assertTrue(testMMU.allocate(p0));
 	}
 
 }
