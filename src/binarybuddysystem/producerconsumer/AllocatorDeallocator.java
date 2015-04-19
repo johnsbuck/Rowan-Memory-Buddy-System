188acba729eeac0755d54220e384151ef51fd3c1
onthered <jbuck594@gmail.com>
2015-04-19 00:29:12 -0400

    Updated classes. Need MainWindow to have method to update internal MMU

--- src/binarybuddysystem/producerconsumer/AllocatorDeallocator.java
+++ src/binarybuddysystem/producerconsumer/AllocatorDeallocator.java
@@ -7,8 +7,8 @@ public class AllocatorDeallocator
 	private static int N;
 	private static int memorySize;
 	private static int minChunk;
-	private static Allocator alloc;
-	private static Deallocator dealloc;
+	private static Allocator alloc = new Allocator();
+	private static Deallocator dealloc = new Deallocator();
 	private static AllocDeallocMonitor mon;
 	private static ArrayList<String> processNames;
 	
@@ -18,8 +18,6 @@ public class AllocatorDeallocator
 		this.minChunk = minChunk;
 		N = memorySize/minChunk;
 		
-		dealloc = new Deallocator();
-		alloc = new Allocator();
 		mon = new AllocDeallocMonitor();
 		
 		processNames = new ArrayList<String>(N);
@@ -27,10 +25,50 @@ public class AllocatorDeallocator
 	
 	static class Allocator implements Runnable
 	{
+		private boolean running;
+		
+		@Override
+		public void run()
+		{
+			running = true;
+			while(running)
+			{
+				mon.allocate();
+			}
+		}
+		
+		public void stop()
+		{
+			running = false;
+		}
+	}
+	
+	static class Deallocator implements Runnable
+	{
+		private boolean running;
+		
+		@Override
+		public void run()
+		{
+			running = true;
+			while(running)
+			{
+				mon.deallocate();
+			}
+		}
+		
+		public void stop()
+		{
+			running = false;
+		}
+	}
+	
+	static class AllocDeallocMonitor
+	{
 		private int chunkVar; //Chunk Variations(variation on chunk size)
 		private int [] chunkLotto;
 		
-		public Allocator()
+		public AllocDeallocMonitor()
 		{
 			//How many possible 2^x are in the MMU
 			chunkVar = (int) (Math.log(N)/Math.log(2)) + 1;
@@ -42,48 +80,21 @@ public class AllocatorDeallocator
 			}
 		}
 		
-		@Override
-		public void run()
-		{
-			while(true)
-			{
-				
-			}
-		}
-		
-		private void deallocateProcess(String name)
-		{
-			
-		}
-	}
-	
-	static class Deallocator implements Runnable
-	{
-		@Override
-		public void run()
-		{
-			while(true)
-			{
-				
-			}
-		}
-		
-		private void allocateProcess(String name, int size)
-		{
-			
-		}
-	}
-	
-	static class AllocDeallocMonitor
-	{
 		public synchronized void allocate()
 		{
+			//If there are no more places for the smallest chunk, wait
+			if(chunkLotto[chunkVar - 1] == 0)
+				sleep();
+			
+			
 			
 		}
 		
 		public synchronized void deallocate()
 		{
-			
+			//If there is enough room to fit the largets chunk, wait
+			if(chunkLotto[0] == 1)
+				sleep();
 		}
 		
 		private void sleep()
