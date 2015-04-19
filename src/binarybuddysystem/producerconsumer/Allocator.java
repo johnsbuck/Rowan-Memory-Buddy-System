110e076683a3a499ce0872eafb45f4d10c8281a3
onthered <jbuck594@gmail.com>
2015-04-18 21:35:16 -0400

    Added Allocator and Deallocator for animated GUI

---
+++ src/binarybuddysystem/producerconsumer/Allocator.java
@@ -0,0 +1,18 @@
+package binarybuddysystem.producerconsumer;
+
+public class Allocator implements Runnable
+{
+	
+	@Override
+	public void run()
+	{
+		try
+		{
+			
+		}
+		catch(InterruptedException e)
+		{
+			
+		}
+	}
+}
---
+++ src/binarybuddysystem/producerconsumer/Deallocator.java
@@ -0,0 +1,19 @@
+package binarybuddysystem.producerconsumer;
+
+public class Deallocator implements Runnable
+{
+	
+	@Override
+	public void run()
+	{
+		try
+		{
+			
+		}
+		catch(InterruptedException e)
+		{
+			
+		}
+	}
+
+}
